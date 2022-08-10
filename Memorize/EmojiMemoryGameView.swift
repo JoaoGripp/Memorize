2f79457ede96b6edd6051b0d604a5d09af81e15e
JoaoGripp <corguinha@gmail.com>
2022-08-10 17:15:48 -0300

    Implicit Amination added - Lecture 07 Done

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -49,10 +49,18 @@ struct CardView: View {
                     endAngle: Angle(degrees: 90 - 90))
                 .padding(5)
                 .opacity(0.5)
-                Text(card.content).font(font(in: geometry.size))
+                Text(card.content)
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
+    }
+    
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private func font(in size: CGSize) -> Font {
@@ -60,6 +68,7 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let fontScale: CGFloat       = 0.7
+        static let fontScale: CGFloat       = 0.65
+        static let fontSize: CGFloat        = 32
     }
 }
